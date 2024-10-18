f50210928f25690f818d77f23d7fce8c3bab4a09
Aqsa-Ali-ally <aqsaalibhayo786>
2024-10-18 12:46:55 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './components/header/header.jsx';
+import Products from './components/products/products.jsx';
 
 
 function App() {
   return (
     <div className="App">
-      
+      < Header />
+     < Products />
     </div>
   );
 }
